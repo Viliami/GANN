2ea047c5fd9bfa78fc84efb6a44fc9eeccf803df
Viliami Tuanaki <Viliami@users.noreply.github.com>
2016-09-20 23:43:14 +1200

    [C++]added Population class

--- CPP/GANNlib.h
+++ CPP/GANNlib.h
@@ -1,4 +1,13 @@
 #include <map>
+#include <vector>
+#include <string>
+#include <random>
+#include <chrono>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <iostream>
 
 class Chromosome{
 public:
@@ -15,6 +24,28 @@ public:
     int getGene(std::string geneName);
     std::string generate();
     std::string mutateFilter(float mutationRate);
+
+private:
+
+};
+
+class Population{
+public:
+    Population();
+    ~Population();
+    std::vector<Chromosome> population;
+    int mutationRate;
+    int crossoverRate;
+    int size;
+    int chromType;
+    int eliteClones;
+
+    Chromosome randomChromosome(); //randomly select individual
+    Chromosome rouletteSelect(); //randomly select individual with chances proportional to fitness
+    Chromosome getFittest(); //get fittest individual
+    std::vector<Chromosome> evolve(); //evolve the current population using GA
+    Chromosome crossover(Chromosome c1, Chromosome c2); //crossover 2 individuals to create a new individual
+    float getAverageFitness(); //get the average fitness of the entire population
 
 private:
 
