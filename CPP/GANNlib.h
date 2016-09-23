d2448e63f3f4157d5fdaedad6e8dbd6861cfbf55
Viliami Tuanaki <Viliami@users.noreply.github.com>
2016-09-23 18:39:35 +1200

    [C++] changed stuff
    mutation rate and crossover rate were ints, causing no crossovers or mutations to happen as 0.01 and 0.7 all converted to 0 as an int.

--- CPP/GANNlib.h
+++ CPP/GANNlib.h
@@ -15,16 +15,17 @@ public:
     int length;
     float fitness;
     std::map<std::string, std::string> genes;
+
     Chromosome();
     Chromosome(std::string chrom);
-    ~Chromosome();
+    ~Chromosome(){};
     bool addGene(std::string geneName, int geneBits);
     bool removeGene(std::string geneName);
-    float (*fitnessFunc)();
+    float fitnessFunc();
     int getGene(std::string geneName);
     std::string generate();
     std::string mutateFilter(float mutationRate);
-
+    void updateGenes();
 private:
 
 };
@@ -32,17 +33,17 @@ private:
 class Population{
 public:
     Population(int size, Chromosome chromType);
-    ~Population();
+    ~Population(){};
     std::vector<Chromosome> population;
-    int mutationRate;
-    int crossoverRate;
+    float mutationRate;
+    float crossoverRate;
     int size;
     int eliteClones;
     Chromosome chromType;
 
     Chromosome randomChromosome(); //randomly select individual
     Chromosome rouletteSelect(); //randomly select individual with chances proportional to fitness
-    Chromosome getFittest(); //get fittest individual
+    Chromosome getFittest(int n); //get fittest individual
     std::vector<Chromosome> evolve(); //evolve the current population using GA
     Chromosome crossover(Chromosome c1, Chromosome c2); //crossover 2 individuals to create a new individual
     float getAverageFitness(); //get the average fitness of the entire population
