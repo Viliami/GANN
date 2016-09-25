76f717ed94a60da2e89597454facff9c0a628d12
Viliami Tuanaki <Viliami@users.noreply.github.com>
2016-09-25 23:36:35 +1300

    [C++] fixed addGene() bug

--- CPP/GANNlib.cpp
+++ CPP/GANNlib.cpp
@@ -44,7 +44,6 @@ Chromosome::Chromosome(std::string chrom){
     this->chrom = chrom;
     this->fitness = 0;
     this->length = this->chrom.length();
-    std::cout << "length: "<<this->length << std::endl;
 }
 
 bool Chromosome::addGene(std::string geneName, int geneBits){
@@ -61,7 +60,8 @@ bool Chromosome::removeGene(std::string geneName){
     try{
         int geneBits = genes[geneName].length();
         this->genes.erase(geneName);
-        for(int i = 0; i < geneBits; i++){
+        for(int i = 0; i < geneBits
+        ; i++){
             this->chrom.pop_back();
         }
         this->length = this->chrom.length();
@@ -71,11 +71,6 @@ bool Chromosome::removeGene(std::string geneName){
         return false;
     }
 }
-
-/*float Chromosome::fitnessFunc(){
-    return 0;
-}*/
-
 int Chromosome::getGene(std::string geneName){
     try{
         return strtoll(this->genes[geneName].c_str(), NULL, 2);
@@ -86,7 +81,6 @@ int Chromosome::getGene(std::string geneName){
 }
 
 std::string Chromosome::generate(){
-    //TODO: update this function to update the genes map
     this->chrom = "";
     for(int i = 0; i < this->length; i++){
         this->chrom+=patch::to_string(randomNumber(0,2));
@@ -103,37 +97,32 @@ void Chromosome::updateGenes(){
     for(int i = 0; i < keys.size(); i++){
         std::string key = keys[i];
         int bitSize = this->genes[key].length();
-        this->genes[key] = this->chrom.substr(cursor,cursor+bitSize);
-        //std::cout << key << ": " << this->genes[key] << std::endl;
+        this->genes[key] = this->chrom.substr(cursor,bitSize);
         cursor+=bitSize;
     }
 }
 
 std::string Chromosome::mutateFilter(float mutationRate){
     std::string chrom = "";
-    //std::cout << "og: " << this->chrom << std::endl;
     for(int i = 0; i < (signed int)this->chrom.length(); i++){
         char bit = this->chrom[i];
-        //std::cout << bit << std::endl;
         float rand = randomFloat();
-        //std::cout << rand << " " << (rand < mutationRate) << mutationRate << std::endl;
         if(rand < mutationRate){
             if(bit == '0'){
                 chrom+='1';
             }else{
                 chrom+='0';
             }
-            //std::cout << chrom << std::endl;
         }else{
             chrom+=bit;
         }
     }
     this->chrom = chrom;
-    //std::cout << "ng: " << this->chrom << std::endl;
     return this->chrom;
 }
 
 Population::Population(int size, Chromosome chromType){
+    srand(GANNlib_random_seed);
     for(int i = 0; i < size; i++){
         Chromosome chrom = Chromosome();
         std::vector<std::string> keys = extract_keys(chromType.genes);
@@ -198,7 +187,6 @@ Chromosome Population::crossover(Chromosome c1, Chromosome c2){
     std::string gene1 = c1.chrom;
     std::string gene2 = c2.chrom;
     int splitIndex = randomNumber(1,gene1.length()-2);
-    //std::cout << "Split index: " << splitIndex << std::endl;
     std::string combinedGene = gene1.substr(0,splitIndex)+gene2.substr(splitIndex,std::string::npos);
     Chromosome chrom = c1;
     chrom.chrom = combinedGene;
@@ -227,6 +215,6 @@ std::vector<Chromosome> Population::evolve(){
 }
 
 float Chromosome::fitnessFunc(){
-    //return this->getGene("x");
-    return 0;
+    //return 0;
+    return this->getGene("x");
 }
